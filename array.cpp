df9e6f1d4f34e599b6bf4b8cd089303d0a9ccb23
liemoren <1344041926@qq.com>
2024-09-01 14:06:41 +0800

    added radixSort

--- array.cpp
+++ array.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
+#include <cstring>
 using namespace std;
 typedef int* pointer;
-
-int  lowest(int* p1,size_t size)//必须在main函数里求得数组大小
+int  lowest(const int* p1,size_t size)//必须在main函数里求得数组大小
                                 // 数组传参到函数里会退化成指针
 {
      int l=p1[0];
@@ -59,27 +59,95 @@ void  InsertionSort(int* a,size_t size)
                 break;
         }
 }
+int Partition(int array[],int low,int high)
+{
+    int pivot=array[high];
+    int i=low-1;
+    for (int j=low;j<high;j++)
+        if (array[j]<pivot)
+        {
+            i++;
+            swap(array[i],array[j]);
+        }
+    swap(array[i+1],array[high]);//i+1前面的都是修改好的
+    return i+1;
+}
 void QuickSort(int array[],int low,int high)
 {
     if (low<high)
     {
-        int PivotIndex=(array,low,high);
+        int PivotIndex=Partition(array,low,high);
         QuickSort(array,low,PivotIndex-1);
         QuickSort(array,PivotIndex+1,high);
     }
 }
-int PivotIndex(int array[],int low,int high)
+void CountingSort(int* a,size_t size)
 {
-    
+    int maxval= a[0];
+    for (int i=1;i<size;i++)
+        if (maxval<a[i])
+            maxval=a[i];
+    int count[maxval+1];
+    for (int i=0;i<maxval+1;i++)
+        count[i]=0;
+    for (int i=0;i<size;i++)
+        count[a[i]]++;
+    int i=0;
+    for (int j=0;j<maxval+1;j++)
+        for (int k=0;k<count[j];k++)
+        {
+            a[i]=j;
+            i++;
+        }
 }
+void RadixSort(int* a,int size)
+{
+    int max_val=a[0];
+    for (int i=1;i<size;i++)
+        if(max_val<a[i])
+            max_val=a[i];
+
+    int RadixArray[10][size];
+    int exp=1;
+    int index;
+    int in;
+    int CountRadix[10]={0};
+    while (max_val/exp!=0)
+    {
+        for (int i=0;i<size;i++)
+        {
+
+            index=(a[i]/exp)%10;//取余
+            RadixArray[index][CountRadix[index]]=a[i];
+            CountRadix[index]++;
+        }
+
+        in=0;
+        for (int j=0;j<10;j++)
+        {
+            for (int i=0;i<CountRadix[j];i++)
+            {
+                a[in]=RadixArray[j][i];
+                in++;
+            }
+            CountRadix[j]=0;
+        }
+        exp*=10;
+    }
+
+}
+
 int main()
 {
-    int a[]={1,0,3,4,5,6,9,8};
+    int a[]={189,0,5,442,63,92,852,3214,264,263,264,332};
     int size1=sizeof(a);//此处求的是数组a的内存大小
     int size=sizeof(a)/sizeof(a[0]);//求数组的大小
     //cout<<lowest(a,size);
     //SelectionSort(a,size);
-    InsertionSort(a,size);
+    //InsertionSort(a,size);
+    //QuickSort(a,0,size-1);
+    //CountingSort(a,size);
+    RadixSort(a,size);
     show(a,size);
     return 0;
 }
